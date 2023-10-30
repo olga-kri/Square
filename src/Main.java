827cad0513bc3050424d3e00f90a7eacbf1d23d3
olga-kri <krivenkoolga1987@gmail.com>
2023-10-31 01:57:02 +0500

    Method square

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
+    // Метод для вычисления квадрата числа
+    public static int square (int a){
+        int result = a*a;
+        return result;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int x = 178;
+        System.out.println("Квадрат числа " + x + " равен " + square(x));
     }
 }
